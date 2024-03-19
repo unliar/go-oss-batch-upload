1848741fec3ffe0e2292558b51c7cb4b83e04f26
远浅 <370732889@qq.com>
2024-03-19 13:40:06 +0800

    feat: add dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,20 @@
-FROM golang:1.18-alpine
+# Set the base build image
+ARG BASE_BUILD_IMAGE=golang:1.22-alpine
+ARG BASE_IMAGE=alpine
 
+# Build stage
+FROM ${BASE_BUILD_IMAGE} as build
 WORKDIR /app
-
 COPY go.mod go.sum ./
-
 RUN go mod download
-
-COPY . .
-
+# Copy all files from current directory to /app in the image
+COPY . /app
 RUN go build -o main
+
+# Production stage
+FROM ${BASE_IMAGE}
+WORKDIR /app
+# Copy the built main file from build stage to production stage
+COPY --from=build /app/main /app/main
 
 CMD ["./main"]
--- main.go
+++ main.go
@@ -3,111 +3,104 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"os"
 	"path/filepath"
 	"strings"
 	"sync"
-
-	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 )
 
-// 定义上传文件的并发数量
-const defaultConcurrency = 10
+type Config struct {
+	AccessKeyID     string
+	AccessKeySecret string
+	Endpoint        string
+	ResourceDir     string
+	PathPrefix      string
+	BucketName      string
+	Concurrency     int
+}
 
-func main() {
-	// 定义命令行参数
-	var accessKeyId string
-	var accessKeySecret string
-	var endpoint string
-	var resourceDir string
-	var pathPrefix string
-	var bucketName string
-	var concurrency int
-
-	flag.StringVar(&accessKeyId, "access-key-id", "", "OSS 账号的 AccessKeyId")
-	flag.StringVar(&accessKeySecret, "access-key-secret", "", "OSS 账号的 AccessKeySecret")
-	flag.StringVar(&endpoint, "endpoint", "", "OSS 的 Endpoint 地址")
-	flag.StringVar(&resourceDir, "resource-dir", "", "资源文件夹路径")
-	flag.StringVar(&pathPrefix, "path-prefix", "", "上传到 OSS 的子目录路径")
-	flag.StringVar(&bucketName, "bucket-name", "", "上传文件的 Bucket 名称")
-	flag.IntVar(&concurrency, "concurrency", defaultConcurrency, "上传文件的并发数量")
-
+func (c *Config) Init() {
+	flag.StringVar(&c.AccessKeyID, "access-key-id", "", "OSS 账号的 AccessKeyId")
+	flag.StringVar(&c.AccessKeySecret, "access-key-secret", "", "OSS 账号的 AccessKeySecret")
+	flag.StringVar(&c.Endpoint, "endpoint", "", "OSS 的 Endpoint 地址")
+	flag.StringVar(&c.ResourceDir, "resource-dir", "", "资源文件夹路径")
+	flag.StringVar(&c.PathPrefix, "path-prefix", "", "上传到 OSS 的子目录路径")
+	flag.StringVar(&c.BucketName, "bucket-name", "", "上传文件的 Bucket 名称")
+	flag.IntVar(&c.Concurrency, "concurrency", 10, "上传文件的并发数量")
 	flag.Parse()
+}
 
-	// 验证参数
-	if accessKeyId == "" || accessKeySecret == "" || endpoint == "" || resourceDir == "" || bucketName == "" || pathPrefix == "" {
-		fmt.Println("Error: Missing required parameters")
-		flag.Usage()
-		return
+func uploadFile(client *oss.Client, path string, resourceDir string, pathPrefix string, bucketName string) error {
+	filename := filepath.Base(path)
+	objectKey := filepath.Join(pathPrefix, filepath.Dir(strings.TrimPrefix(path, resourceDir)), filename)
+	fmt.Println("File upload Ready", objectKey)
+	bk, err := client.Bucket(bucketName)
+	if err != nil {
+		fmt.Println("Error creating OSS bucket:", err)
+		return err
 	}
+	err = bk.PutObjectFromFile(objectKey, path)
+	if err != nil {
+		fmt.Println("Error uploading file:", err)
+		return err
+	} else {
+		fmt.Println("File uploaded successfully:", objectKey)
+		return nil
+	}
+}
 
-	// 获取 OSS 客户端
+func createOSSClient(c *Config) *oss.Client {
 	client, err := oss.New(
-		endpoint,
-		accessKeyId,
-		accessKeySecret,
+		c.Endpoint,
+		c.AccessKeyID,
+		c.AccessKeySecret,
 	)
 	if err != nil {
 		fmt.Println("Error creating OSS client:", err)
-		return
+		os.Exit(1)
 	}
+	return client
+}
 
-	// 创建一个通道，用于存储待上传的文件
-	files := make(chan string, concurrency)
-
-	// 并发上传
+func uploadFiles(c *Config, client *oss.Client) {
+	files := make(chan string, c.Concurrency)
 	var wg sync.WaitGroup
-	wg.Add(concurrency)
-	for i := 0; i < concurrency; i++ {
+	wg.Add(c.Concurrency)
+	for i := 0; i < c.Concurrency; i++ {
 		go func() {
 			for path := range files {
-				// 获取文件名称
-				filename := filepath.Base(path)
-
-				// 拼接 OSS 对象键
-				objectKey := filepath.Join(pathPrefix, filepath.Dir(strings.TrimPrefix(path, resourceDir)), filename)
-				fmt.Println("File upload Ready", objectKey)
-				// 上传文件
-				bk, err := client.Bucket(bucketName)
+				err := uploadFile(client, path, c.ResourceDir, c.PathPrefix, c.BucketName)
 				if err != nil {
-					fmt.Println("Error uploading Bucket:", err)
-				} else {
-					err = bk.PutObjectFromFile(objectKey, path)
-					if err != nil {
-						fmt.Println("Error uploading file:", err)
-					} else {
-						fmt.Println("File uploaded successfully:", objectKey)
-					}
-
+					fmt.Println("Error uploading file:", err)
+					os.Exit(1)
 				}
 			}
-
 			wg.Done()
 		}()
 	}
-
-	// 遍历资源文件夹，将文件路径发送到通道
-	err = filepath.Walk(resourceDir, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(c.ResourceDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-
-		// 判断是否是文件
 		if !info.IsDir() {
 			files <- path
 		}
-
 		return nil
 	})
-
 	if err != nil {
 		fmt.Println("Error walking directory:", err)
 		return
 	}
-
 	close(files)
-
-	// 等待所有文件上传完成
 	wg.Wait()
-	fmt.Println("All files uploaded successfully!")
+	fmt.Println("Uploaded all files successfully!")
+}
+
+func main() {
+	c := Config{}
+	c.Init()
+
+	client := createOSSClient(&c)
+	uploadFiles(&c, client)
 }
