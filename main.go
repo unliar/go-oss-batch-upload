09a5648e0ab1518247f37485e08d6c10468285d9
远浅 <370732889@qq.com>
2024-03-19 14:01:02 +0800

    feat: add dockerfile

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
@@ -9,6 +10,8 @@ import (
 	"strings"
 	"sync"
 )
+
+const ConcurrencyDefault = 10
 
 type Config struct {
 	AccessKeyID     string
@@ -27,8 +30,37 @@ func (c *Config) Init() {
 	flag.StringVar(&c.ResourceDir, "resource-dir", "", "资源文件夹路径")
 	flag.StringVar(&c.PathPrefix, "path-prefix", "", "上传到 OSS 的子目录路径")
 	flag.StringVar(&c.BucketName, "bucket-name", "", "上传文件的 Bucket 名称")
-	flag.IntVar(&c.Concurrency, "concurrency", 10, "上传文件的并发数量")
+	flag.IntVar(&c.Concurrency, "concurrency", ConcurrencyDefault, "上传文件的并发数量")
 	flag.Parse()
+	flag.Usage()
+	if c.AccessKeyID == "" {
+		handleError(errors.New("-access-key-id is required"), "OSS 账号的 AccessKeyId 不能为空")
+	}
+	if c.AccessKeySecret == "" {
+		handleError(errors.New("-access-key-secret is required"), "OSS 账号的 AccessKeySecret 不能为空")
+	}
+	if c.Endpoint == "" {
+		handleError(errors.New("-endpoint is required"), "OSS 的 Endpoint 地址不能为空")
+	}
+	if c.ResourceDir == "" {
+		handleError(errors.New("-resource-dir is required"), "资源文件夹路径不能为空")
+	}
+	if c.PathPrefix == "" {
+		handleError(errors.New("-path-prefix is required"), "上传到 OSS 的子目录路径不能为空")
+	}
+	if c.BucketName == "" {
+		handleError(errors.New("-bucket-name is required"), "上传文件的 Bucket 名称不能为空")
+	}
+	if c.Concurrency <= 0 {
+		handleError(errors.New("-concurrency must be more than 0"), "上传文件的并发数量不能小于0")
+	}
+}
+
+func handleError(err error, message string) {
+	if err != nil {
+		fmt.Println(message, err)
+		os.Exit(1)
+	}
 }
 
 func uploadFile(client *oss.Client, path string, resourceDir string, pathPrefix string, bucketName string) error {
@@ -36,18 +68,14 @@ func uploadFile(client *oss.Client, path string, resourceDir string, pathPrefix 
 	objectKey := filepath.Join(pathPrefix, filepath.Dir(strings.TrimPrefix(path, resourceDir)), filename)
 	fmt.Println("File upload Ready", objectKey)
 	bk, err := client.Bucket(bucketName)
-	if err != nil {
-		fmt.Println("Error creating OSS bucket:", err)
-		return err
-	}
+	handleError(err, "Error creating OSS bucket:")
+
 	err = bk.PutObjectFromFile(objectKey, path)
 	if err != nil {
-		fmt.Println("Error uploading file:", err)
-		return err
-	} else {
-		fmt.Println("File uploaded successfully:", objectKey)
-		return nil
+		handleError(err, "Error uploading file:")
 	}
+	fmt.Println("File uploaded successfully:", objectKey)
+	return nil
 }
 
 func createOSSClient(c *Config) *oss.Client {
@@ -56,10 +84,7 @@ func createOSSClient(c *Config) *oss.Client {
 		c.AccessKeyID,
 		c.AccessKeySecret,
 	)
-	if err != nil {
-		fmt.Println("Error creating OSS client:", err)
-		os.Exit(1)
-	}
+	handleError(err, "Error creating OSS client:")
 	return client
 }
 
@@ -70,28 +95,19 @@ func uploadFiles(c *Config, client *oss.Client) {
 	for i := 0; i < c.Concurrency; i++ {
 		go func() {
 			for path := range files {
-				err := uploadFile(client, path, c.ResourceDir, c.PathPrefix, c.BucketName)
-				if err != nil {
-					fmt.Println("Error uploading file:", err)
-					os.Exit(1)
-				}
+				_ = uploadFile(client, path, c.ResourceDir, c.PathPrefix, c.BucketName)
 			}
 			wg.Done()
 		}()
 	}
 	err := filepath.Walk(c.ResourceDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
 		if !info.IsDir() {
 			files <- path
 		}
-		return nil
+		return err
 	})
-	if err != nil {
-		fmt.Println("Error walking directory:", err)
-		return
-	}
+	handleError(err, "Error walking directory:")
+
 	close(files)
 	wg.Wait()
 	fmt.Println("Uploaded all files successfully!")
@@ -100,7 +116,6 @@ func uploadFiles(c *Config, client *oss.Client) {
 func main() {
 	c := Config{}
 	c.Init()
-
 	client := createOSSClient(&c)
 	uploadFiles(&c, client)
 }
